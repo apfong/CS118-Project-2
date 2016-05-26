f107e617f086691035c50eaea397e6d75862d5ce
brandonpon96 <brandonpon96@gmail.com>
2016-05-25 18:36:51 -0700

    not working yet cries

--- client.cpp
+++ client.cpp
@@ -1,13 +1,27 @@
-#include <string>
+#include <string.h>
 #include <thread>
 #include <iostream>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <stdio.h>
+#include <errno.h>
+#include <unistd.h>
+#include <netdb.h>
+#include <fstream>
+using namespace std;
 
-int main()
+int main(int argc, char* argv[])
 {
-	char * buf = new char[1024];
+	//char * buf = new char[1024];
 
 	// create a socket using TCP IP
-  	int sockfd = socket(AF_INET, SOCK_STREAM, 0);
+  	int sockfd = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
+  if(sockfd == -1){
+  	perror("bad socket");
+  	return 1;
+  }
 
   	 struct addrinfo hints;
 	  struct addrinfo* res;
@@ -17,6 +31,10 @@ int main()
 	  hints.ai_socktype = SOCK_STREAM; // TCP
 	  // gets a list of addresses stored in res
 	  int status = 0;
+	  string hoststring = "localhost";
+	  const char * host = hoststring.c_str();
+	  string portstring = "4000";
+	  const char * port = portstring.c_str();
 	  if ((status = getaddrinfo(host, port, &hints, &res)) != 0) {
 	    std::cerr << "getaddrinfo: " << gai_strerror(status) << std::endl;
 	    return 2;
@@ -29,7 +47,24 @@ int main()
     // convert the IP to a string and print it:
     char ipstr[INET_ADDRSTRLEN] = {'\0'};
     inet_ntop(p->ai_family, &(ipv4->sin_addr), ipstr, sizeof(ipstr));
+    cout <<"ipaddr "<<ipstr<<endl;
+	//create a struct sockaddr with the given port and ip address
+	struct sockaddr_in serverAddr;
+	serverAddr.sin_family = AF_INET;
+	serverAddr.sin_port = htons(4000);//request.getPort();
+	//serverAddr.sin_port = htons(80);     // short, network byte order
+	serverAddr.sin_addr.s_addr = inet_addr(ipstr);
+	memset(serverAddr.sin_zero, '\0', sizeof(serverAddr.sin_zero));
 
-	sendto(sockfd, buf, 1024, 0, addr, length);
-
+	while(true){
+		string msg;
+		cin >> msg;
+		if(sendto(sockfd, &msg[0], msg.size(), 0, (struct sockaddr *)&serverAddr, (socklen_t)sizeof(serverAddr))==-1){
+			perror("send error");
+			return 1;
+		}
+		cout<<"sent"<<endl;
+	}
+	close(sockfd);
+	return 0;
 }
--- server.cpp
+++ server.cpp
@@ -1,9 +1,65 @@
-#include <string>
+#include <string.h>
 #include <thread>
 #include <iostream>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <stdio.h>
+#include <errno.h>
+#include <unistd.h>
+#include <netdb.h>
+#include <fstream>
+using namespace std;
 
 int main()
 {
-  std::cerr << "web server is not implemented yet" << std::endl;
-  // do your stuff here! or not if you don't want to.
+    // create a socket using TCP IP
+  int sockfd = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
+  if(sockfd == -1){
+  	perror("bad socket");
+  	return 1;
+  }
+  int port = 4000;
+  // allow others to reuse the address
+  int yes = 1;
+  if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int)) == -1) {
+    perror("setsockopt");
+    return 1;
+  }
+
+  // bind address to socket
+  struct sockaddr_in addr;
+  addr.sin_family = AF_INET;
+  addr.sin_port = htons(port);     // short, network byte order
+  addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+  memset(addr.sin_zero, '\0', sizeof(addr.sin_zero));
+
+  if (bind(sockfd, (struct sockaddr*)&addr, sizeof(addr)) == -1) {
+    perror("bind");
+    return 2;
+  }
+
+  // set socket to listen status
+  // if (listen(sockfd, 1) == -1) {   // how many connections the queue will hold onto
+  //   perror("listen");
+  //   return 3;
+  // }
+  char * buf = new char[1024];
+	memset(buf,'\0',sizeof(buf));
+  	cout<<"start recv"<<endl;
+	struct sockaddr_in clientAddr;
+    socklen_t clientAddrSize = sizeof(clientAddr);
+
+  while(true){
+  	
+  	if(recvfrom(sockfd, buf, 1024, 0, (struct sockaddr*)&clientAddr, &clientAddrSize) == -1){
+  		perror("error receive");
+  		return 1;
+  	}
+  	cout<<"received"<<buf<<endl;
+
+  }
+  close(sockfd);
+  return 0;
 }
